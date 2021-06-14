6a97d453cd9745edff12ddf2aaa38196aeb74171
1317Stephen <1317stephen@gmail.com>
2021-06-14 21:11:56 +0900

    [Daily] 2021-06-13

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "[Daily] 2021-06-12 SAT",
-        "excerpt":"Jekyll Blog      Based on minimal-mistakes   Current Blog structure            Posts: Section for the project-related recording       Daily: Section for the daily study record           Future Challenges           Language switch: English ⇔ Korean              language switcher (Github)       Making Jekyll multilingual                Add daily graph on Daily (similar with Github commit graph)         ","categories": [],
+        "excerpt":"Jekyll Blog Based on minimal-mistakes Current Blog structure Posts: Section for the project-related recording Daily: Section for the daily study record Other references minimal-mistakes docs devinlife blog &amp; devinlife-git Bugs #4 build issues: Github-page build issue ⇒ Add css/js &amp; package files Future Challenges Language switch: English ⇔ Korean language...","categories": [],
         "tags": [],
         "url": "/daily/2021-06-12/",
         "teaser": null
