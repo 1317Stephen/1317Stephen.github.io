367f6090fb4c7f9395a69c1162561bfb8fcca2a7
1317Stephen <1317stephen@gmail.com>
2021-06-21 21:19:33 +0900

    [daily] 2021-06-16 WED

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,16 @@ var store = [{
         "tags": [],
         "url": "/daily/2021-06-14/",
         "teaser": null
-      },]
+      },{
+        "title": "[Daily] 2021-06-16 WED",
+        "excerpt":"Physics Quantum Information/Quantum Computation Book: Quantum Computation and Quantum Information Lectures Quantum Computation and Quantum Information 2018 lecture (included Youtube lecture) Quantum Computation 2004 lecture (Umesh Vazirani) Papers MA Nielsen, I Chuang, Quantum Computation and Quantum Information Quantum simulator List of Open Quantum Projects Game Game Engine Programming : I...","categories": [],
+        "tags": [],
+        "url": "/daily/2021-06-16/",
+        "teaser": null
+      },{
+        "title": "Edge Case: Nested and Mixed Lists",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "/edge%20case/edge-case-nested-and-mixed-lists/",
+        "teaser": null
+      }]
