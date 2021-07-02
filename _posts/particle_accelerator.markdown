3fb4146de0903c5f0f7220215df1a9441cfa9cf9
1317Stephen <1317stephen@gmail.com>
2021-07-02 11:15:04 +0900

    [Daily] Jun 2021 (2)

---
+++ _posts/particle_accelerator.markdown
@@ -0,0 +1,59 @@
+---
+# layout: posts
+title: "Webtoon Application(iOS)"
+date: 2021-07-01 11:12:34 +0900
+tags:
+  - iOS Application
+excerpt: "Based on existing webtoons applications, we are going to produce an iOS sample webtoon application as a test."
+---
+
+## Webtoon Market Research
+<!-- picture -->
+reference: [Webtoon market share (Android)][Webtoon market share]
+- Naver Webtoon (5,521,674 users)
+- KakaoPage (2,411,481 users)
+
+## Common Features
+- Bottom Navigation Bar
+- Main Page: Weekly Webtoons (+ Novels...)
+- My Page: Webtoons I've seen.
+- View More: Setting, Cookie/Cash, Notice...
+
+## Naver Webtoon vs KakaoPage
+
+
+
+## Jekyll Blog
+
+- Based on [minimal-mistakes][minimal-mistakes]
+- Current Blog structure
+  - `Posts`: Section for the project-related recording
+  - `Daily`: Section for the daily study record
+- Other references
+  - [minimal-mistakes docs][minimal-mistakes docs]
+  - [devinlife blog][devinlife blog] & [devinlife-git][devinlife-git]
+
+### Bugs
+
+- [#4 build issues][#4 build issues]: Github-page build issue ⇒ Add css/js & package files
+
+## Future Challenges
+
+- Language switch: English ⇔ Korean
+
+  - [language switcher (Github)][language switcher (github)]
+  - [Making Jekyll multilingual][making jekyll multilingual]
+
+- Add daily graph on `Daily` (similar with Github commit graph)
+
+<!--
+{% highlight ruby %}
+def print_hi(name)
+puts "Hi, #{name}"
+end
+print_hi('Tom')
+#=> prints 'Hi, Tom' to STDOUT.
+{% endhighlight %}
+ -->
+
+[Webtoon market share]: https://platum.kr/archives/155134
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,14 +12,26 @@ var store = [{
         "teaser": null
       },{
         "title": "[Daily] 2021-06-16 WED",
-        "excerpt":"Physics Quantum Information/Quantum Computation Book: Quantum Computation and Quantum Information Lectures Quantum Computation and Quantum Information 2018 lecture (included Youtube lecture) Quantum Computation 2004 lecture (Umesh Vazirani) Papers MA Nielsen, I Chuang, Quantum Computation and Quantum Information Quantum simulator List of Open Quantum Projects Game Game Engine Programming : I...","categories": [],
+        "excerpt":"Physics Quantum Information/Quantum Computation Book: Quantum Computation and Quantum Information Lectures Quantum Computation and Quantum Information 2018 lecture (included Youtube lecture) Quantum Computation 2004 lecture (Umesh Vazirani) Papers MA Nielsen, I Chuang, Quantum Computation and Quantum Information Quantum simulator List of Open Quantum Projects List of QC simulators Game Game...","categories": [],
         "tags": [],
         "url": "/daily/2021-06-16/",
         "teaser": null
       },{
-        "title": "Edge Case: Nested and Mixed Lists",
-        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
-        "tags": ["content","css","edge case","lists","markup"],
-        "url": "/edge%20case/edge-case-nested-and-mixed-lists/",
+        "title": "[Daily] 2021-06-21 MON",
+        "excerpt":"Movie Recommendation Plan: 6/28 LeetCode Today: String 1451. Rearrange Words in a Sentence map bucket solution Better Solution: additional stringstream &amp; map stringstream while(ss &gt;&gt; word) for(auto p: m) rs &lt;&lt; p.second; map: (same length of word) add to end of the string m[word.size()] += word + \" \"; Reminder...","categories": [],
+        "tags": [],
+        "url": "/daily/2021-06-21/",
+        "teaser": null
+      },{
+        "title": "[Daily] 2021-06-23 WED",
+        "excerpt":"LeetCode Today: Swift 1. Two Sum Swift basic functions Summary Array var arr : [Int] = [] var arr2 = [Int]() var arr3 : Array&lt;Int&gt; = [] arr.append(2) For for i in arr {} for i in 0...n {} for i in 0..&lt;n {} for i in (0...n).reversed() {} for...","categories": [],
+        "tags": [],
+        "url": "/daily/2021-06-23/",
+        "teaser": null
+      },{
+        "title": "Start Website based on ",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Server"],
+        "tags": ["Oracle Cloud","Spring Boot","Node.js","Vue.js","Oracle ATP Database","Backend","Frontend"],
+        "url": "/server/edge-case-nested-and-mixed-lists/",
         "teaser": null
       }]
